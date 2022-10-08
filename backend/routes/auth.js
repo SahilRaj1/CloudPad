ef2158cdc1a744f82f5149817de3c2956fd75a10
SahilRaj1 <raj24sahil@gmail.com>
2022-10-08 12:26:35 +0530

    CHANGES MADE: { added user validation, unique email-ids }

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
 
-router.post('/', (req, res) => {
+router.post('/', [
+    body('email', 'Enter a valid email').isEmail(),
+    body('name', 'Enter a valid name').isLength({ min: 3 }),
+    body('password', 'Password should contain atleast 8 characters').isLength({ min: 8 })
+], (req, res) => {
 
-    const user = User(req.body);
-    user.save();
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-    res.json(req.body);
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      }).then(user => res.json(user))
+      .catch(err => {
+        console.log(err);
+        res.json({error: "Email already used"});
+    });
+
 });
 
 module.exports = router

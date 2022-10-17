4c4c4415dec8b34b0f881c336d1f2ce4bf347dc0
SahilRaj1 <raj24sahil@gmail.com>
2022-10-17 10:15:58 +0530

    navbar and router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
-  return (
-    <>
-      
-    </>
-  );
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route exact path="/" element={<Home/>}></Route>
+                <Route exact path="/about" element={<About/>}></Route>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
